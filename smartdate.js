fd038ab4bdb5d0fa3a0b0f74af34ff6839769c53
Denis Stebunov <denis.stebunov@ivelum.com>
2014-10-29 14:04:49 +0300

    version 0.2.0

---
+++ smartdate.js
@@ -0,0 +1,159 @@
+/* global define */
+(function(window) {
+  'use strict';
+
+  var smartdate = {
+    version: '0.2.0',
+
+    language: 'en',
+
+    tagName: 'span',
+
+    className: 'smartdate',
+
+    timestampAttr: 'timestamp',
+
+    addTitle: true
+  };
+
+  var daysBetween = smartdate.daysBetween = function(date1, date2) {
+    // Copy date parts of the timestamps, discarding the time parts.
+    var one = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate()),
+        two = new Date(date2.getFullYear(), date2.getMonth(), date2.getDate());
+
+    // Do the math.
+    var millisecondsPerDay = 1000 * 60 * 60 * 24,
+        millisBetween = two.getTime() - one.getTime(),
+        days = millisBetween / millisecondsPerDay;
+
+    // Round down.
+    return Math.floor(days);
+  };
+
+  var pad = smartdate.pad = function(n) {
+    return n < 10 ? '0' + n : n;
+  };
+
+  var formatAMPM = smartdate.formatAMPM = function(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = pad(minutes);
+    return hours + ':' + minutes + ' ' + ampm;
+  };
+
+  smartdate.locale = {
+    'en': {
+      dateFormat: function(dt) {
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        return months[dt.getMonth()] + ' ' +
+            dt.getDate() + ', ' +
+            dt.getFullYear();
+      },
+
+      sinceNow: function(date) {
+        var now = new Date();
+        var sec = (now.getTime() - date.getTime()) / 1000;
+
+        if (sec < 60) {
+          return 'less than a minute ago';
+        } else if (sec < 60 * 60) {
+          return Math.floor(sec / 60) + ' min ago';
+        } else if (daysBetween(now, date) === 0) {
+          return 'today at ' + formatAMPM(date);
+        } else if (daysBetween(now, date) === -1) {
+          return 'yesterday at ' + formatAMPM(date);
+        }
+      }
+    },
+
+    'ru': {
+      dateFormat: function(dt) {
+        var months = ['янв', 'фев', 'мар', 'апр', 'мая', 'июн',
+                      'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
+        return dt.getDate() + ' ' +
+            months[dt.getMonth()] + ' ' +
+            dt.getFullYear();
+      },
+
+      sinceNow: function(date) {
+        var now = new Date();
+        var sec = (now.getTime() - date.getTime()) / 1000;
+
+        if (sec < 60) {
+          return 'менее 1 мин назад';
+        } else if (sec < 60 * 60) {
+          return Math.floor(sec / 60) + ' мин назад';
+        } else if (daysBetween(now, date) === 0) {
+          return 'сегодня в ' + pad(date.getHours()) +
+              ':' + pad(date.getMinutes());
+        } else if (daysBetween(now, date) === -1) {
+          return 'вчера в ' + pad(date.getHours()) +
+              ':' + pad(date.getMinutes());
+        }
+      }
+    }
+  };
+
+  smartdate.getLocale = function(language) {  // 'en' is default
+    if (typeof language === 'string') {
+      language = language.toLowerCase();
+      if (smartdate.locale.hasOwnProperty(language)) {
+        return smartdate.locale[language];
+      }
+    }
+    return smartdate.locale['en'];
+  };
+
+  smartdate.sinceNow = function(date, language) {  // language is optional
+    return smartdate.getLocale(language).sinceNow(date);
+  };
+
+  smartdate.dateFormat = function(date, language) {  // language is optional
+    return smartdate.getLocale(language).dateFormat(date);
+  };
+
+  smartdate.removeClass = function(el, className) {  // Works in IE8+
+    if (el.classList) {
+      el.classList.remove(className);
+    } else {
+      el.className = el.className.replace(
+          new RegExp('(^|\\b)' +
+              className.split(' ').join('|') +
+              '(\\b|$)', 'gi'), ' '
+      );
+    }
+  };
+
+  smartdate.render = function() {
+    var selector = smartdate.tagName + '.' + smartdate.className;
+    var elements = document.querySelectorAll(selector);
+    Array.prototype.forEach.call(elements, function(el) {
+      var timestamp = el.getAttribute('data-' + smartdate.timestampAttr);
+      var date = new Date(timestamp * 1000);
+
+      var dateText = smartdate.sinceNow(date);
+      if (!dateText) {
+        dateText = smartdate.dateFormat(date);
+        smartdate.removeClass(el, smartdate.className);
+      }
+
+      el.innerHTML = dateText;
+      if (smartdate.addTitle) {
+        el.setAttribute('title', date.toLocaleString());
+      }
+    });
+  };
+
+  if (typeof define === 'function' && define.amd) {
+    define('smartdate', [], function() {
+      return smartdate;
+    });
+  } else {
+    window.smartdate = smartdate;
+  }
+
+})(window);
